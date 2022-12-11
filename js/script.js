e896febb5e294fb9aca52c463316f2ab982620fe
murtazajoo <98375522+murtazajoo@users.noreply.github.com>
2022-12-11 23:15:51 +0530

    merged

--- js/script.js
+++ js/script.js
@@ -1,49 +1,335 @@
-import btns from "./source.js";
-let main = document.getElementById("main");
-let bsColors = {
-  css: "primary",
-  diagram: "secondary",
-  image: "success",
-  illustration: "success",
-  icon: "danger",
-  fonts: "danger",
-  js: "warning",
-  html: "info",
-  design: "dark",
-};
-let finalHTML = "";
-
-btns.map((e) => {
-  let cato = e.category;
-  let color = bsColors[cato];
-  let dis = e.discription;
-  let title = e.text;
-  let link = e.link;
-  finalHTML += `
-    <button  
-    type="button"
-    class="btn btn-${color} mx-4 my-4  p-3 text-dark " 
-    data-bs-container="body"
-     data-bs-toggle="popover"
-      data-bs-placement="top" 
-      data-bs-content="${dis}"
-        data-bs-trigger="hover"
- 
- >
-   <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-${color}">
-     ${cato}
-   </span>
-  <a href="${link}" target="_blank" class="text-${color} text-decoration-none fw-bold"> ${title} <br> (<small class="text-dark">${link}</small>)</a>
- </button>;
- 
-  `;
-});
-
-main.innerHTML = finalHTML;
-
-const popoverTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="popover"]'
-);
-const popoverList = [...popoverTriggerList].map(
-  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-);
+const _0x188004 = _0x3797;
+(function (_0x50ea77, _0x5761df) {
+  const _0xe40b7a = _0x3797,
+    _0x1d1ecb = _0x50ea77();
+  while (!![]) {
+    try {
+      const _0x210861 =
+        (-parseInt(_0xe40b7a(0xa9)) / 0x1) *
+          (-parseInt(_0xe40b7a(0x9f)) / 0x2) +
+        -parseInt(_0xe40b7a(0xd0)) / 0x3 +
+        (-parseInt(_0xe40b7a(0xba)) / 0x4) * (parseInt(_0xe40b7a(0xdb)) / 0x5) +
+        (-parseInt(_0xe40b7a(0xde)) / 0x6) *
+          (-parseInt(_0xe40b7a(0xbe)) / 0x7) +
+        (-parseInt(_0xe40b7a(0xb0)) / 0x8) *
+          (-parseInt(_0xe40b7a(0xc5)) / 0x9) +
+        -parseInt(_0xe40b7a(0xed)) / 0xa +
+        parseInt(_0xe40b7a(0x97)) / 0xb;
+      if (_0x210861 === _0x5761df) break;
+      else _0x1d1ecb["push"](_0x1d1ecb["shift"]());
+    } catch (_0x3f5c56) {
+      _0x1d1ecb["push"](_0x1d1ecb["shift"]());
+    }
+  }
+})(_0x84df, 0xe51a4);
+function _0x3797(_0x13b6c7, _0x11954c) {
+  const _0x84dfc9 = _0x84df();
+  return (
+    (_0x3797 = function (_0x3797ff, _0x3cbf7e) {
+      _0x3797ff = _0x3797ff - 0x8f;
+      let _0x3023c1 = _0x84dfc9[_0x3797ff];
+      return _0x3023c1;
+    }),
+    _0x3797(_0x13b6c7, _0x11954c)
+  );
+}
+let btns = [
+    {
+      category: _0x188004(0xc6),
+      text: _0x188004(0xd3),
+      link: _0x188004(0xa7),
+      discription:
+        "Start\x20your\x20web\x20project\x20by\x20generating\x20and\x20downloading\x20your\x20custom\x20HTML\x20Boilerplate.",
+    },
+    {
+      category: _0x188004(0xec),
+      text: _0x188004(0xda),
+      link: _0x188004(0xbd),
+      discription: _0x188004(0xc8),
+    },
+    {
+      category: _0x188004(0xec),
+      text: "\x20\x20BOX-SHADOW",
+      link: _0x188004(0xe0),
+      discription: _0x188004(0x96),
+    },
+    {
+      category: "css",
+      text: _0x188004(0xb1),
+      link: _0x188004(0xee),
+      discription: _0x188004(0xa8),
+    },
+    {
+      category: _0x188004(0xec),
+      text: _0x188004(0xad),
+      link: "https://autoprefixer.github.io/",
+      discription:
+        "adds\x20the\x20prefix\x20for\x20evry\x20browser,\x20makes\x20css\x20more\x20flexable",
+    },
+    {
+      category: _0x188004(0xec),
+      text: "SVG\x20WAVES",
+      link: "https://getwaves.io/",
+      discription:
+        "a\x20free\x20svg\x20wave\x20generator\x20to\x20make\x20unique\x20svg\x20waves\x20for\x20your\x20next\x20web\x20design.\x20choose\x20a\x20curve,\x20adjust\x20complexity,\x20randomize!.",
+    },
+    {
+      category: _0x188004(0x91),
+      text: _0x188004(0xd7),
+      link: _0x188004(0xc2),
+      discription:
+        "Google\x20Fonts\x20is\x20a\x20library\x20of\x201474\x20open\x20source\x20font\x20families\x20and\x20APIs\x20for\x20convenient\x20use\x20via\x20CSS\x20and\x20Android",
+    },
+    {
+      category: _0x188004(0xa4),
+      text: "FONTAEESOME",
+      link: _0x188004(0x90),
+      discription: _0x188004(0xdd),
+    },
+    {
+      category: "css",
+      text: _0x188004(0xbf),
+      link: _0x188004(0xc0),
+      discription: _0x188004(0xc1),
+    },
+    {
+      category: "image",
+      text: _0x188004(0xd9),
+      link: _0x188004(0xcd),
+      discription: _0x188004(0xe3),
+    },
+    {
+      category: _0x188004(0xcf),
+      text: _0x188004(0xce),
+      link: _0x188004(0x9c),
+      discription: _0x188004(0xe4),
+    },
+    {
+      category: "image",
+      text: _0x188004(0xd4),
+      link: _0x188004(0xb2),
+      discription: _0x188004(0x98),
+    },
+    {
+      category: "image",
+      text: _0x188004(0xc4),
+      link: _0x188004(0xd8),
+      discription: _0x188004(0x9b),
+    },
+    {
+      category: _0x188004(0xec),
+      text: _0x188004(0xa3),
+      link: _0x188004(0xef),
+      discription: _0x188004(0xc9),
+    },
+    {
+      category: "css",
+      text: _0x188004(0xae),
+      link: _0x188004(0xdf),
+      discription: _0x188004(0xb3),
+    },
+    {
+      category: _0x188004(0xec),
+      text: _0x188004(0xab),
+      link: "https://animista.net/",
+      discription: _0x188004(0xe9),
+    },
+    {
+      category: _0x188004(0x94),
+      text: _0x188004(0x99),
+      link: _0x188004(0x95),
+      discription: _0x188004(0xac),
+    },
+    {
+      category: "design",
+      text: _0x188004(0xbb),
+      link: _0x188004(0xb8),
+      discription: _0x188004(0xa1),
+    },
+    {
+      category: _0x188004(0xaf),
+      text: "DRIBBBLE",
+      link: "https://dribbble.com/",
+      discription: _0x188004(0xa1),
+    },
+    {
+      category: _0x188004(0xcf),
+      text: _0x188004(0x8f),
+      link: _0x188004(0xe7),
+      discription:
+        "This\x20is\x20a\x20site\x20where\x20you\x20can\x20freely\x20download\x20images,\x20illustrations,\x20videos\x20and\x20vectors.\x20These\x20images\x20are\x20freely\x20licensed\x20for\x20commercial\x20use.",
+    },
+    {
+      category: _0x188004(0xcf),
+      text: _0x188004(0x9a),
+      link: _0x188004(0xb4),
+      discription: _0x188004(0xe6),
+    },
+    {
+      category: "image",
+      text: _0x188004(0xea),
+      link: _0x188004(0xaa),
+      discription: _0x188004(0xd5),
+    },
+    {
+      category: _0x188004(0xc7),
+      text: "DIAGRAMS",
+      link: _0x188004(0x93),
+      discription: _0x188004(0xe8),
+    },
+    {
+      category: "js",
+      text: _0x188004(0xdc),
+      link: "https://obfuscator.io/",
+      discription: _0x188004(0xc3),
+    },
+  ],
+  main = document[_0x188004(0xcc)](_0x188004(0xeb)),
+  bsColors = {
+    css: _0x188004(0xca),
+    diagram: _0x188004(0xd2),
+    image: "success",
+    illustration: _0x188004(0xb7),
+    icon: "danger",
+    fonts: _0x188004(0x92),
+    js: "warning",
+    html: "info",
+    design: _0x188004(0xe1),
+  },
+  finalHTML = "";
+btns[_0x188004(0xa2)]((_0x1f96b4) => {
+  const _0x55dfca = _0x188004;
+  let _0x509b98 = _0x1f96b4[_0x55dfca(0xe2)],
+    _0x3e6a2a = bsColors[_0x509b98],
+    _0x176b4d = _0x1f96b4["discription"],
+    _0x4882be = _0x1f96b4[_0x55dfca(0xcb)],
+    _0x1234e3 = _0x1f96b4[_0x55dfca(0xb9)];
+  finalHTML +=
+    "\x0a\x20\x20\x20\x20<button\x20\x20\x0a\x20\x20\x20\x20type=\x22button\x22\x0a\x20\x20\x20\x20class=\x22btn\x20btn-" +
+    _0x3e6a2a +
+    _0x55dfca(0xd6) +
+    _0x176b4d +
+    _0x55dfca(0xb5) +
+    _0x3e6a2a +
+    _0x55dfca(0x9d) +
+    _0x509b98 +
+    _0x55dfca(0xa6) +
+    _0x1234e3 +
+    _0x55dfca(0xd1) +
+    _0x3e6a2a +
+    _0x55dfca(0xa5) +
+    _0x4882be +
+    _0x55dfca(0xb6) +
+    _0x1234e3 +
+    "</small>)</a>\x0a\x20</button>;\x0a\x20\x0a\x20\x20";
+}),
+  (main[_0x188004(0x9e)] = finalHTML);
+const popoverTriggerList = document[_0x188004(0xa0)](_0x188004(0xe5)),
+  popoverList = [...popoverTriggerList][_0x188004(0xa2)](
+    (_0x1460b1) => new bootstrap[_0x188004(0xbc)](_0x1460b1)
+  );
+function _0x84df() {
+  const _0x5beaf3 = [
+    "COMPRESS\x20JPEG\x20|\x20PNG\x20|\x20..",
+    "21663JCJhBo",
+    "html",
+    "diagram",
+    "a\x20handpicked\x20collection\x20of\x20beautiful\x20color\x20gradients\x20for\x20designers\x20and\x20developers.",
+    "This\x20site\x20aids\x20you\x20with\x20divers\x20and\x20a\x20variety\x20of\x20colors\x20for\x20your\x20designs",
+    "primary",
+    "text",
+    "getElementById",
+    "https://www.remove.bg/upload",
+    "PHOTORESIZER",
+    "image",
+    "26145lGnnks",
+    "\x22\x20target=\x22_blank\x22\x20class=\x22text-",
+    "secondary",
+    "\x20\x20\x20\x20\x20HTML\x20BOILERPLATE",
+    "FREE\x20LOGO\x20DESIGN",
+    "Unsplash\x20is\x20similar\x20to\x20other\x20image\x20providers,\x20have\x20a\x20large\x20variety\x20of\x20images\x20and\x20an\x20advanced\x20search\x20bar.\x20",
+    "\x20mx-4\x20my-4\x20\x20p-3\x20text-dark\x20\x22\x20\x0a\x20\x20\x20\x20data-bs-container=\x22body\x22\x0a\x20\x20\x20\x20\x20data-bs-toggle=\x22popover\x22\x0a\x20\x20\x20\x20\x20\x20data-bs-placement=\x22top\x22\x20\x0a\x20\x20\x20\x20\x20\x20data-bs-content=\x22",
+    "Google\x20Fonts",
+    "https://compressjpeg.com/",
+    "REMOVE.BG",
+    "CSS\x20GRADIENT",
+    "6272485HtfTbp",
+    "OBFUSCATOR",
+    "Font\x20Awesome\x20is\x20the\x20Internet\x27s\x20icon\x20library\x20and\x20toolkit,\x20used\x20by\x20millions\x20of\x20designers,\x20developers,\x20and\x20content\x20creators.",
+    "1050978bkPOOS",
+    "https://cssgenerator.org/",
+    "https://getcssscan.com/css-box-shadow-examples",
+    "dark",
+    "category",
+    "Remove\x20Image\x20Background",
+    "Resize\x20&\x20crop\x20your\x20photos\x20online",
+    "[data-bs-toggle=\x22popover\x22]",
+    "Similar\x20to\x20Pixabay,\x20This\x20site\x20offers\x20pictures\x20and\x20images\x20of\x20various\x20sizes\x20and\x20orientations\x20for\x20your\x20site\x20free\x20of\x20charge.",
+    "https://pixabay.com/",
+    "This\x20tool\x20is\x20very\x20important\x20for\x20prototyping\x20and\x20designs.\x20It\x20can\x20help\x20you\x20to\x20draw\x20your\x20front-end\x20design\x20structure\x20if\x20you\x20may\x20want\x20to\x20share\x20ideas\x20with\x20your\x20friends.",
+    "This\x20tools\x20aid\x20you\x20to\x20generate\x20css\x20animations\x20and\x20automatically\x20get\x20the\x20CSS\x20equivalent\x20code.",
+    "UNSPLASH",
+    "main",
+    "css",
+    "9847100nQPvFt",
+    "https://uiverse.io/",
+    "https://www.materialpalette.com/",
+    "PIXABAY",
+    "https://fontawesome.com/",
+    "fonts",
+    "danger",
+    "https://app.diagrams.net/",
+    "illustration",
+    "https://undraw.co/illustrations",
+    "get\x2099+\x20box-shadow\x20examples,\x20just\x20click\x20to\x20copy\x20them",
+    "11397276eKfOUa",
+    "Create\x20your\x20logo\x20design\x20with\x20our\x20free\x20logo\x20maker\x20in\x20few\x20minutes",
+    "UNDRAW",
+    "PEXELS",
+    "Select\x20up\x20to\x2020\x20JPG\x20or\x20JPEG\x20images\x20from\x20you\x20device\x20Download\x20compressed\x20images\x20either\x20separately\x20or\x20get\x20them\x20all,\x20grouped\x20in\x20a\x20ZIP\x20archive",
+    "https://www.photoresizer.com/",
+    "\x22>\x0a\x20\x20\x20\x20\x20",
+    "innerHTML",
+    "2478106biDUGY",
+    "querySelectorAll",
+    "gives\x20you\x20a\x20variety\x20of\x20designs\x20for\x20development.\x20You\x20can\x20search\x20for\x20your\x20designs\x20as\x20well\x20as\x20find\x20designs\x20based\x20on\x20the\x20type\x20of\x20device;\x20Mobile\x20or\x20web.",
+    "map",
+    "MATERIAL\x20PALETTE",
+    "icon",
+    "\x20text-decoration-none\x20fw-bold\x22>\x20",
+    "\x0a\x20\x20\x20</span>\x0a\x20\x20<a\x20href=\x22",
+    "https://htmlboilerplates.com/",
+    "we\x20give\x20anyone\x20the\x20power\x20to\x20create,\x20share,\x20and\x20use\x20beautiful\x20custom\x20elements\x20made\x20with\x20css\x20and\x20html.",
+    "1oVmtLO",
+    "https://unsplash.com/",
+    "ANIMISTA",
+    "Undraw\x20gives\x20you\x20a\x20tone\x20of\x20illustrations\x20for\x20your\x20design.\x20It\x20has\x20a\x20search\x20bar\x20where\x20you\x20can\x20query\x20based\x20on\x20what\x20you\x20are\x20designing.\x20",
+    "\x20\x20AUTOPREFIXER\x20-CSS\x20",
+    "CSS\x20GENERATOR",
+    "design",
+    "1864ayOTQN",
+    "CUSTOM\x20ELEMENTS\x20",
+    "https://www.freelogodesign.org/",
+    "Automatically\x20generate\x20your\x20CSS\x20gradient\x20with\x20a\x20suitable\x20GUI.",
+    "https://www.pexels.com/",
+    "\x22\x0a\x20\x20\x20\x20\x20\x20\x20\x20data-bs-trigger=\x22hover\x22\x0a\x20\x0a\x20>\x0a\x20\x20\x20<span\x20class=\x22position-absolute\x20top-0\x20start-50\x20translate-middle\x20badge\x20rounded-pill\x20bg-",
+    "\x20<br>\x20(<small\x20class=\x22text-dark\x22>",
+    "success",
+    "https://www.uplabs.com/",
+    "link",
+    "4phTIAy",
+    "UPLABS",
+    "Popover",
+    "https://uigradients.com/",
+    "14vCMkRs",
+    "NEUMORPHISM",
+    "https://neumorphism.io/#e0e0e0",
+    "Generate\x20Soft-UI\x20CSS\x20code",
+    "https://fonts.google.com",
+    "A\x20free\x20and\x20efficient\x20obfuscator\x20for\x20JavaScript\x20(including\x20support\x20of\x20ES2022).\x20Make\x20your\x20code\x20harder\x20to\x20copy\x20and\x20prevent\x20people\x20from\x20stealing\x20your\x20work.",
+  ];
+  _0x84df = function () {
+    return _0x5beaf3;
+  };
+  return _0x84df();
+}
